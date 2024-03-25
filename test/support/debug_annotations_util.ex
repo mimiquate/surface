2088c4d47c09874fc9cb8f1a36db16e4baa8717b
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-03-25 14:47:11 -0300

    test: fix test helper for lv >= 0.20.10

--- test/support/debug_annotations_util.ex
+++ test/support/debug_annotations_util.ex
@@ -1,13 +1,16 @@
 defmodule Surface.CompilerTest.DebugAnnotationsUtil do
   def debug_heex_annotations_supported? do
-    Application.spec(:phoenix_live_view, :vsn)
-    |> to_string()
-    |> Version.parse!()
+    phoenix_live_view_version()
     |> Version.compare("0.20.0") != :lt
   end
 
+  def debug_heex_annotations_option_supported? do
+    phoenix_live_view_version()
+    |> Version.match?(">= 0.20.0 and <= 0.20.9")
+  end
+
   defmacro use_component() do
-    if __MODULE__.debug_heex_annotations_supported?() do
+    if __MODULE__.debug_heex_annotations_option_supported?() do
       quote do
         use Surface.Component, debug_heex_annotations: true
       end
@@ -16,5 +19,11 @@ defmodule Surface.CompilerTest.DebugAnnotationsUtil do
         use Surface.Component
       end
     end
+  end
+
+  defp phoenix_live_view_version do
+    Application.spec(:phoenix_live_view, :vsn)
+    |> to_string()
+    |> Version.parse!()
   end
 end
